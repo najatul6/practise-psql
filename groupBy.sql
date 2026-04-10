24fd782ee968b9700b66544cf1fb6558e68c1d60
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-04-10 21:02:00 +0600

    Add SQL queries for student data analysis, including grouping by country and birth year

---
+++ groupBy.sql
@@ -0,0 +1,23 @@
+
+SELECT * from students;
+SELECT country, count(*), avg(age) from students 
+    GROUP BY country;
+SELECT country, avg(age) from students 
+    GROUP BY country
+       HAVING  avg(age) > 20.60
+    ;
+
+
+
+
+-- Filter Groups Using HAVING to Show Only Countries with Average Age Above 20.60
+SELECT country, avg(age) from students 
+    GROUP BY country
+       HAVING  avg(age) > 20.60
+    ;
+
+
+-- Count Students Born in Each Year
+SELECT extract(year from dob) as birth_year, count(*)
+    FROM students
+    GROUP BY birth_year
