ef90e853390bb482ac7b7a38fb9176b8b6de3dc4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-04-25 14:00:59 +0600

    Update and asign some function for add some text

--- node/logger-app/index.js
+++ node/logger-app/index.js
@@ -1,13 +1,18 @@
-const path=require("path")
-const inputArguments=process.argv.slice(2)
+const path = require("path");
+const fs = require("fs");
 
-const text =inputArguments.join(' ')
-if(!text){
-    console.log("❌ Please Add some text")
-    console.log("Example: node index.js Hello World")
-    process.exit(1)
+const inputArguments = process.argv.slice(2);
+const text = inputArguments.join(" ").concat("\n");
+const message = `${text},\n`;
+
+if (!message) {
+  console.log("❌ Please Add some log");
+  console.log("Example: node index.js Hello World");
+  process.exit(1);
 }
 
-const filePath=path.join(__dirname, "log.txt")
-
-console.log(filePath)
+const filePath = path.join(__dirname, "log.txt");
+fs.appendFile(filePath, message, "utf-8", () => {
+  console.log("")
+});
+console.log(filePath);
